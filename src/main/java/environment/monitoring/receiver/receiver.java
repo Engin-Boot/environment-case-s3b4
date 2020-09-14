c6cf708c9d49fd4eb3db3df5f3de134bc53a0bd6
anagheshm <anagheshm@gmail.com>
2020-09-14 13:19:01 +0530

    resolve issues

---
+++ src/main/java/environment/monitoring/receiver/receiver.java
@@ -0,0 +1,12 @@
+package environment.monitoring.receiver;
+
+public class receiver {
+		
+		public static void main( String[] args )
+	    {
+	        System.out.println( "check statement via receiver" );
+	    }
+	}
+	
+	
+
--- src/main/java/sender/Application.java
+++
@@ -1,8 +0,0 @@
-package sender;
-
-public class Application {
-	public static void main(String[] args) {
-		System.out.println("Sender sends");
-		//comment added
-	}
-}
