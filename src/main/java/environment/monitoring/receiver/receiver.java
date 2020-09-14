fbaed733bc098077927e7ea4048d503a111402e2
sanjeev-nnn <sanjeev.kumar@philips.com>
2020-09-14 13:31:07 +0530

    branch create commit

--- src/main/java/environment/monitoring/receiver/receiver.java
+++ src/main/java/environment/monitoring/receiver/receiver.java
@@ -4,7 +4,7 @@ public class receiver {
 		
 		public static void main( String[] args )
 	    {
-	        System.out.println( "check statement via receiver" );
+	        System.out.println( "check via receiver" );
 	    }
 	}
 	
