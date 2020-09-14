f1db98522eee5e070720a279f71ee1304a40bbd2
Sanjeev kumar <sanjeevnnn@gmail.com>
2020-09-14 12:54:17 +0530

    Add code for initial checking

---
+++ src/main/java/environment/monitoring/sender/sender.java
@@ -0,0 +1,9 @@
+package environment.monitoring.sender;
+
+public class sender
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "check statement from sender!" );
+    }
+}
