84fea55f32116c0aa5783f7f83b049fbb1f45b86
sanjeev-nnn <sanjeev.kumar@philips.com>
2020-09-18 00:07:17 +0530

    Reduce complexity by breaking warnErrorCheck method

--- receiver/src/main/java/com/main/BreachLevel.java
+++ receiver/src/main/java/com/main/BreachLevel.java
@@ -1,5 +1,5 @@
 package com.main;
-import java.io.InputStream;
+import java.io.*;
 import java.util.*;
 
 import java.util.Map;
@@ -12,19 +12,23 @@ public class BreachLevel {
 	int[] humidityErrorLimit = {-1 , 90};
 	
 	 boolean isOperatingConditionsOk(Map<String, Integer> environmentConditionAttributes){
-		
+				
 		boolean flag=true;
 		int temp=environmentConditionAttributes.get("temperature");
 		int humid=environmentConditionAttributes.get("humidity");
 		
 		environmentConditionAttributes.forEach((k, v) -> {
+			
+			
 			System.out.println(k + " : " + (v + 10));
 		});
 		
-		flag &= warnAndErrorCheck(temp, tempWarnLimit[0], tempErrorLimit[0], tempWarnLimit[1], tempErrorLimit[1]);
-		flag &= warnAndErrorCheck(humid, humidityWarnLimit[0], humidityErrorLimit[0], humidityWarnLimit[1], humidityErrorLimit[1]);
+		flag &= warnCheck(temp, tempWarnLimit[0], tempWarnLimit[1]);
+		flag &= errorCheck(temp, tempErrorLimit[0], tempErrorLimit[1]);
+		flag &= warnCheck(humid, humidityWarnLimit[0], humidityWarnLimit[1]);
+		flag &= errorCheck(humid, humidityErrorLimit[0], humidityErrorLimit[1]);
 		return flag;
-	}
+		}
 	
 /*
 	 boolean temperatureBreachCheck(int temp)
@@ -45,7 +49,40 @@ public class BreachLevel {
 		return false;
 	}
 */	
-	boolean warnAndErrorCheck(int val, int warnLow, int errorLow, int warnHigh, int errorHigh)
+	 boolean warnCheck(int val, int warnLow, int warnHigh)
+	 {
+		 if(val < warnLow)
+	        {
+	            System.out.println("There is a WARN level LOW limit breach in the " + Thread.currentThread().getStackTrace()[2].getMethodName());
+	            return false;
+	        }
+		 
+		 if(val > warnHigh)
+	        {
+	            System.out.println("There is a WARN level HIGH limit breach in the " + Thread.currentThread().getStackTrace()[2].getMethodName());
+	            return false;
+	        }
+		 return true;
+	 }
+	 
+	 boolean errorCheck(int val, int errorLow, int errorHigh)
+	 {
+		 if(val < errorLow)
+	        {
+	            System.out.println("There is a ERROR level LOW limit breach in the " + Thread.currentThread().getStackTrace()[2].getMethodName());
+	            return false;
+	        }
+		 
+		 if(val > errorHigh)
+	        {
+	            System.out.println("There is a ERROR level HIGH limit breach in the " + Thread.currentThread().getStackTrace()[2].getMethodName());
+	            return false;
+	        }
+		 return true;
+	 }
+	 
+/*
+	 boolean warnAndErrorCheck(int val, int warnLow, int errorLow, int warnHigh, int errorHigh)
 	{
 		if(warnLow != -1){
 			if(val < errorLow)
@@ -75,5 +112,5 @@ public class BreachLevel {
 		
 		return true;
 	}
-
+*/
 }
